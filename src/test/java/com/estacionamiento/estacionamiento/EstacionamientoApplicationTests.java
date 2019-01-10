49501464f722982417c4e4a9e1a8eb39cdec6a4b
jhonatan gomez <jhonatan.gomez@ceiba.com.co>
2019-01-10 15:48:00 -0500

    .

--- src/test/java/com/estacionamiento/estacionamiento/EstacionamientoApplicationTests.java
+++ src/test/java/com/estacionamiento/estacionamiento/EstacionamientoApplicationTests.java
@@ -4,11 +4,14 @@ import static org.junit.Assert.assertArrayEquals;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@EnableAutoConfiguration(exclude = HibernateJpaAutoConfiguration.class)
 public class EstacionamientoApplicationTests {
 
 	@Test
